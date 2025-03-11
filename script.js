ba9b74ab029c98183554cc476ed4f508878f344e
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-12 00:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 //your JS code here. If required.
 
-let form = document.querySelector('form');
-let userBox = document.getElementById('username');
-let passwordBox = document.getElementById('password');
-let checkBox = document.getElementById('checkbox');
-let btn = document.getElementById('submit');
+document.addEventListener("DOMContentLoaded" , function () {
+	const submitButton = document.getElementById('submit');
+	const existingUserButton = document.getElementById('existing')
+	const userInput = document.getElementById('username');
+	const passwordInput = document.getElementById('password');
+	const rememberMeCheckbox = document.getElementById('checkbox');
 
-form.addEventListener("submit" , (events)=>{
-	events.preventDefault():
-	let username = userBox.value;
-	alert(`Logged in as ${username}`)
-	let password = passwordBox.value;
-	
-	if(checkBox.required === false){
-		localStorage.remove();
+	const savedCredentials = localStorage.getItem('userCredentials');
+	if(savedCredentials){
+		existingUserButton.classList.remove("hidden");
 	}
+
+	submitButton.addEventListener("click" , (event)=> {
+		
+		event.preventDefault();
+		const username = userInput.value;
+		const password = passwordInput.value;
+
+		alert(`Logged in as ${username}`);
+
+		if(rememberMeCheckbox.checked){
+			localStorage.setItem("userCredentials" , JSON.stringigy({username , password}))
+		}else{
+			localStorage.remove("userCredentials");
+		}
+
+		
+	})
+
+	existingUserButton.addEventListener("click" , ()=> {
+		const credentials = JSON.parse(localStorage.getItem("userCredentials"));
+		if(credentials){
+			alert(`Logged in as ${credentials.username`);
+		}
+	})
 })
-
-
 
 
 
