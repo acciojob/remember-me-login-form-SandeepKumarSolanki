87bd1da42c69f69329f899a8d2ab30c8cbd1bd12
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-11 23:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let form = document.querySelector('form');
+let userBox = document.getElementById('username');
+let passwordBox = document.getElementById('password');
+let checkBox = document.getElementById('checkbox');
+let btn = document.getElementById('submit');
+
+form.addEventListener("submit" , (events)=>{
+	events.preventDefault():
+	let username = userBox.value;
+	alert(`Logged in as ${username}`)
+	let password = passwordBox.value;
+	
+	if(checkBox.required === false){
+		localStorage.remove();
+	}
+})
+
+
+
+
+
+
